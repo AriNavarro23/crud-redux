e1ab45e69ea3bf4281e41b11faaa2a115b02708c
Ariel Navarro <navarro.a.ariel@hotmail.com>
2023-04-20 17:07:19 -0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { Routes, Route } from "react-router-dom";
-import UserList from "./features/UserList";
+import { Route, Routes } from "react-router-dom";
 import AddUser from "./features/AddUser";
+import EditUser from "./features/EditUser";
+import UserList from "./features/UserList";
 
 function App() {
   return (
     <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
-      <h1 className="text-center font-bold text-2xl text-gray-700">Crud with redux toolkit</h1>
+      <h1 className="text-center font-bold text-2xl text-gray-700">CRUD with redux toolkit</h1>
       <Routes>
         <Route path="/" element={<UserList />} />
         <Route path="/add-user" element={<AddUser />} />
+        <Route path="/edit-user/:id" element={<EditUser />} />
       </Routes>
     </div>
   );
